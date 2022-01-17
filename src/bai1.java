d0b0b49a07e058ad56220208a21bbb3a7407a2c7
DoTienAnh <handes11020@gmail.com>
2022-01-17 16:49:55 +0700

    BaiThucHanhjava

--- src/bai1.java
+++ src/bai1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class App {
+public class bai1 {
     public static void main(String[] args) throws Exception {
         // doi tuong de nhap xuat
         Scanner sc = new Scanner(System.in);
